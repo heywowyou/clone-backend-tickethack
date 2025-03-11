790a0faf1855a4cb97ddac91e23d9e7ee2a5ff97
heywowyou <pro@julesfontaine.com>
2025-03-11 11:29:26 +0100

    Create get trips by names function

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
-const Trip = require('../models/trips');
+const mongoose = require("mongoose");
+const Trip = require("../models/trips");
 
 /* GET All trips */
-router.get('/trips', function(req, res, next) {
-res.status(200).json({message: "All good"});
+router.get("/trips", function (req, res, next) {
+  res.status(200).json({ message: "All good" });
 });
 
+// Get trips by names
+router.get("/trips/:departure/:arrival", async function (req, res) {
+  const { departure, arrival } = req.params;
 
-//get trips by names 
-router.get('/trips/:departure/:arrival', function(req, res, next) {
+  // Find trips using departure and arrival data
+  const trips = await Trip.find({ departure, arrival });
 
+  if (trips.length === 0) {
+    return res.status(404).json({ message: "No trips found." });
+  }
 
+  res.status(200).json(trips);
 });
-
-
 
 module.exports = router;
