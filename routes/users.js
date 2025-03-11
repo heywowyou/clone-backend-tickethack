94632d753c1dff75c05a94426569a6b22f29183b
heywowyou <pro@julesfontaine.com>
2025-03-11 15:06:44 +0100

    Create route to generate fake users and get all users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const User = require("../models/users");
+const Trip = require("../models/trips");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Get all users
+router.get("/", async function (req, res) {
+  const users = await User.find();
+
+  if (users.length === 0) {
+    return res.status(404).json({ message: "No users found." });
+  }
+
+  res.status(200).json({ users });
 });
+
+
+/* Generate fake user
+router.post("/create", async (req, res) => {
+  const username = "Alice";
+  const password = "Elk";
+  const allTrips = await Trip.find();
+
+  const shuffledTrips = allTrips.sort(() => 0.5 - Math.random());
+  const cart = shuffledTrips.slice(0, 2).map((trip) => trip._id);
+  const bookedTrips = shuffledTrips.slice(2, 4).map((trip) => trip._id);
+
+  const user = new User({
+    username,
+    password,
+    cart,
+    bookedTrips,
+  });
+
+  await user.save();
+  res.json({ message: "User created successfully", user });
+}); */
 
 module.exports = router;
