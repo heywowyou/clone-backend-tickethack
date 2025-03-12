756e5e40643dd056d04f76ccefa6d80a090071bb
heywowyou <pro@julesfontaine.com>
2025-03-12 16:46:47 +0100

    Format connection.js file

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString = 'mongodb+srv://warren9555:xr6Z5bEVJumU7zzq@cluster0.kh3se.mongodb.net/TicketHack';
+const connectionString =
+  "mongodb+srv://warren9555:xr6Z5bEVJumU7zzq@cluster0.kh3se.mongodb.net/TicketHack";
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error));
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.error(error));
